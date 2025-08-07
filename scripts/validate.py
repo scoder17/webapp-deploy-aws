6d0abc1b160b8bc62928f65b36f8f72f31dbd268
Adarsh <starcoder17@gmail.com>
2025-08-07 18:41:06 +0530

    removed unnecessary comments

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,16 +1,3 @@
-# #!/usr/bin/env python3
-# import urllib.request
-
-# try:
-#     response = urllib.request.urlopen("http://localhost")
-#     if response.status == 200:
-#         print("App is healthy")
-#     else:
-#         raise Exception("Unhealthy")
-# except Exception as e:
-#     print("Validation failed:", e)
-#     exit(1)
-
 #!/usr/bin/env python3
 import urllib.request
 import time
